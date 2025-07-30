11c29fd8e21066a85b00f253994a1761168aab77
lokeshkumar-2003 <kumarlokesh37492@gmail.com>
2025-07-30 09:16:27 +0530

    update url in axios

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://foodx-system.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
